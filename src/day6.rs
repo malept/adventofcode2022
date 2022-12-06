2bcdef87402284202614f0bcbc5e61697acafadd
Mark Lee <adventofcode@lazymalevolence.com>
2022-12-05 23:20:59 -0800

    Day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -1,23 +1,21 @@
 use std::collections::HashSet;
 
+const MARKER_LENGTH: usize = 14;
+
 pub fn find_start_of_packet_marker(datastream: &str) -> Option<usize> {
     let count = datastream.len();
     let chars: Vec<char> = datastream.chars().collect();
     for (idx, c) in chars.iter().enumerate() {
-        if idx < 3 {
-            continue;
-        }
-        if idx + 3 >= count {
+        if idx + MARKER_LENGTH - 1 >= count {
             break;
         }
-        let unique: HashSet<char> = HashSet::from([
-            c.clone(),
-            chars[idx + 1].clone(),
-            chars[idx + 2].clone(),
-            chars[idx + 3].clone(),
-        ]);
-        if unique.len() == 4 {
-            return Some(idx + 1);
+        let mut unique: HashSet<char> = HashSet::with_capacity(MARKER_LENGTH);
+        unique.insert(c.clone());
+        for n in 1..MARKER_LENGTH {
+            unique.insert(chars[idx + n].clone());
+        }
+        if unique.len() == MARKER_LENGTH {
+            return Some(idx + MARKER_LENGTH);
         }
     }
 
@@ -33,10 +31,10 @@ mod tests {
 
     #[test]
     fn test_find_start_of_packet_marker() {
-        assert_eq!(super::find_start_of_packet_marker(SIGNAL1), Some(7));
-        assert_eq!(super::find_start_of_packet_marker(SIGNAL2), Some(5));
-        assert_eq!(super::find_start_of_packet_marker(SIGNAL3), Some(6));
-        assert_eq!(super::find_start_of_packet_marker(SIGNAL4), Some(10));
-        assert_eq!(super::find_start_of_packet_marker(SIGNAL5), Some(11));
+        assert_eq!(super::find_start_of_packet_marker(SIGNAL1), Some(19));
+        assert_eq!(super::find_start_of_packet_marker(SIGNAL2), Some(23));
+        assert_eq!(super::find_start_of_packet_marker(SIGNAL3), Some(23));
+        assert_eq!(super::find_start_of_packet_marker(SIGNAL4), Some(29));
+        assert_eq!(super::find_start_of_packet_marker(SIGNAL5), Some(26));
     }
 }
